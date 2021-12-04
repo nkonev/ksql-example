fa246265cc848cb569fb20db9d5fa618c72c54f2
Nikita Konev <nikit.cpp@yandex.ru>
2021-12-04 13:15:20 +0300

    consumer app is 17

--- consumer-spring/src/main/java/io/tpd/kafkaexample/UserChangeDto.java
+++ consumer-spring/src/main/java/io/tpd/kafkaexample/UserChangeDto.java
@@ -4,47 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDateTime;
 
-public class UserChangeDto {
+public record UserChangeDto (
     @JsonProperty("REGISTERTIME")
-    private LocalDateTime registerTime;
+    LocalDateTime registerTime,
     @JsonProperty("GENDER")
-    private String gender;
+    String gender,
     @JsonProperty("REGIONID")
-    private String regionId;
-
-    public UserChangeDto() {
-    }
-
-    public LocalDateTime getRegisterTime() {
-        return registerTime;
-    }
-
-    public void setRegisterTime(LocalDateTime registerTime) {
-        this.registerTime = registerTime;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getRegionId() {
-        return regionId;
-    }
-
-    public void setRegionId(String regionId) {
-        this.regionId = regionId;
-    }
-
-    @Override
-    public String toString() {
-        return "UserChangeDto{" +
-                " registertime=" + registerTime +
-                ", gender='" + gender + '\'' +
-                ", regionid='" + regionId + '\'' +
-                '}';
-    }
-}
+    String regionId
+){}
