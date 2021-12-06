665a257dba915844197bfc4b1611a4299b0108a1
Nikita Konev <nikit.cpp@yandex.ru>
2021-12-06 17:53:41 +0300

    add logging

--- producer-service/src/main/java/com/example/kafkotest/UploadController.java
+++ producer-service/src/main/java/com/example/kafkotest/UploadController.java
@@ -1,6 +1,8 @@
 package com.example.kafkotest;
 
 import io.tpd.kafkaexample.CoordinatesDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -11,8 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class UploadController {
@@ -22,19 +22,22 @@ public class UploadController {
 
     private static final String COMMA_DELIMITER = ",";
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UploadController.class);
+
     @Value("${coordinates.topic-name}")
     private String coordinatesTopicName;
 
     @PostMapping("/upload")
     public void post(MultipartFile file) throws IOException {
-        //file.getBytes()
         try (BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(COMMA_DELIMITER);
                 // https://yandex.ru/map-constructor/location-tool/?from=club
-                // carId, latitude, longitude
-                kafkaTemplate.send(coordinatesTopicName, values[0], new CoordinatesDto(Double.parseDouble(values[1]), Double.parseDouble(values[2])));
+                final var carId = values[0];
+                final var coordinatesDto = new CoordinatesDto(Double.parseDouble(values[1]), Double.parseDouble(values[2]));
+                LOGGER.info("Producing carId={}, coordinatesDto={}", carId, coordinatesDto);
+                kafkaTemplate.send(coordinatesTopicName, carId, coordinatesDto);
             }
         }
     }
