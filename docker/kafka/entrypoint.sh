ec94377af7013486fc0524bd6ab5a4efbb1f40b5
Nikita Konev <nikit.cpp@yandex.ru>
2022-02-14 01:36:28 +0300

    producer-consumer working

--- docker/kafka/entrypoint.sh
+++ docker/kafka/entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-format_done=/bitnami/kafka/format_done
+format_done=/tmp/kafka-data/format_done
 
 if [[ ! -f $format_done ]]; then
   echo "Will format kafka-storage for KRaft"
-
-#  mkdir -p /opt/bitnami/kafka/conf
-#  touch /bitnami/kafka/config/kafka.properties
 
   # KRaft required step: Format the storage directory with a new cluster ID
   kafka_storage=/opt/bitnami/kafka/bin/kafka-storage.sh
@@ -17,7 +14,5 @@ else
   echo "kafka-storage is already formatted, skipping"
 fi
 
-echo "Starting original run script"
-exec /opt/bitnami/scripts/kafka/entrypoint.sh /opt/bitnami/kafka/bin/kafka-server-start.sh /opt/bitnami/kafka/config/kraft/server.properties
-# ./bin/kafka-server-start.sh ./config/kraft/server.properties
-#sleep 1000000
+echo "Starting non-original run script"
+exec /opt/bitnami/kafka/bin/kafka-server-start.sh /opt/bitnami/kafka/config/kraft/server.properties
