72f2bc78ad633a064801ce8237a4f41bb2442160
Nikita Konev <nikit.cpp@yandex.ru>
2022-02-14 20:26:39 +0300

    use bitnami's kafka_configure_from_environment_variables() from /opt/bitnami/scripts/libkafka.sh to write env vars to properties

--- docker/kafka/entrypoint.sh
+++ docker/kafka/entrypoint.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+. /opt/bitnami/scripts/libfile.sh
+. /opt/bitnami/scripts/liblog.sh
+. /opt/bitnami/scripts/libkafka.sh
+
+config_done=/var/lib/kafka-data/config_done
+
+if [[ ! -f $config_done ]]; then
+  echo "Will edit config"
+  # altering config
+  # KAFKA_CONF_FILE is from /opt/bitnami/scripts/kafka-env.sh
+  export KAFKA_CONF_FILE=/opt/bitnami/kafka/config/kraft/server.properties
+
+  kafka_configure_from_environment_variables
+
+  touch $config_done
+else
+  echo "config is already edited, skipping"
+fi
+
+
 format_done=/var/lib/kafka-data/format_done
 
 if [[ ! -f $format_done ]]; then
@@ -13,6 +33,7 @@ if [[ ! -f $format_done ]]; then
 else
   echo "kafka-storage is already formatted, skipping"
 fi
+
 
 echo "Starting non-original run script"
 exec /opt/bitnami/kafka/bin/kafka-server-start.sh /opt/bitnami/kafka/config/kraft/server.properties
