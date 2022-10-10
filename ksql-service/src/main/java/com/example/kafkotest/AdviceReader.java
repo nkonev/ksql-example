db8ce378518c02d234965ff35b822aff8778f9dc
Nikita Konev <nikit.cpp@yandex.ru>
2022-10-11 01:51:32 +0400

    Imports

--- ksql-service/src/main/java/com/example/kafkotest/AdviceReader.java
+++ ksql-service/src/main/java/com/example/kafkotest/AdviceReader.java
@@ -1,22 +1,13 @@
 package com.example.kafkotest;
 
 import io.confluent.ksql.api.client.Client;
-import io.confluent.ksql.api.client.Row;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ExitCodeEvent;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.context.event.ApplicationFailedEvent;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 
 @Component
 class AdviceReader implements KsqldbClientCustomizer, ApplicationContextAware {
