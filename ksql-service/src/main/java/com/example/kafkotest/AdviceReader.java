c84daf062a684662406d26cfa03701c333661fe2
Nikita Konev <nikit.cpp@yandex.ru>
2022-10-11 01:50:48 +0400

    Shutdown KsqlApplication in case error

--- ksql-service/src/main/java/com/example/kafkotest/AdviceReader.java
+++ ksql-service/src/main/java/com/example/kafkotest/AdviceReader.java
@@ -6,12 +6,24 @@ import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ExitCodeEvent;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.event.ApplicationFailedEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
 @Component
-class AdviceReader implements KsqldbClientCustomizer {
+class AdviceReader implements KsqldbClientCustomizer, ApplicationContextAware {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AdviceReader.class);
+
+    private ApplicationContext applicationContext;
 
     @Override
     public void accept(Client client) {
@@ -23,7 +35,13 @@ class AdviceReader implements KsqldbClientCustomizer {
                     streamedQueryResult.subscribe(subscriber);
                 }).exceptionally(e -> {
                     LOGGER.error("Request failed: " + e);
-                    throw new RuntimeException(e);
+                    SpringApplication.exit(applicationContext, () -> 1);
+                    return null;
                 });
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
